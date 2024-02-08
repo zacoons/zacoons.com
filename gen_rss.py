014723aae1533300ace277d460a05eb28b54e7b5
zac <zac@zacoons.com>
2024-02-08 16:20:44 +1000

    moved title and description metadata to page html
    allowed for simple markdown pages
    
    moved gen_rss to its own file

---
+++ gen_rss.py
@@ -0,0 +1,32 @@
+from utils import get_posts
+from urllib.parse import quote_plus
+
+with open("public/rss.xml", "w") as f:
+    f.writelines([
+        "<?xml version=\"1.0\" encoding=\"utf-8\"?>",
+        "<rss xmlns:atom=\"http://www.w3.org/2005/Atom\" version=\"2.0\">",
+        "<channel>",
+            "<atom:link href=\"https://zacoons.com/rss.xml\" rel=\"self\" type=\"application/rss+xml\"/>"
+            "<title>zacoons' blog</title>",
+            "<link>https://zacoons.com/blog</link>",
+            "<description>Recent posts on zacoons' blog</description>",
+            "<language>en</language>",
+    ])
+    for post in get_posts(5):
+        datestr = post.date.strftime("%a, %d %b %Y %H:%M:%S")
+        f.writelines([
+            "<item>",
+                "<title>{0}</title>".format(post.title),
+                "<link>https://zacoons.com/blog/{0}</link>".format(quote_plus(post.postname)),
+                "<guid>https://zacoons.com/blog/{0}</guid>".format(quote_plus(post.postname)),
+                "<pubDate>{0} +0000</pubDate>".format(datestr),
+                "<author>zac@zacoons.com (Zac)</author>",
+                "<description><![CDATA[",
+                *post.html.splitlines(),
+                "]]></description>",
+            "</item>"
+        ])
+    f.writelines([
+        "</channel>",
+        "</rss>"
+    ])
